86654e99f6a5cf82ac9a98757b4a431569bd8658
Arif <faizin.arief@gmail.com>
2020-05-08 14:08:11 +0700

    Fix : unused variable

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -1,14 +1,10 @@
 package com.dicoding.tourismapp.favorite
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
-import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.GetFavoriteTourismUseCase
-import com.dicoding.tourismapp.core.domain.Tourism
 import com.dicoding.tourismapp.core.domain.UseCase
 
 class FavoriteViewModel(private val getFavoriteTourismUseCase: GetFavoriteTourismUseCase) : ViewModel() {
-    fun getFavoriteTourism(): LiveData<List<Tourism>> = getFavoriteTourismUseCase.run(UseCase.None())
+    fun getFavoriteTourism() = getFavoriteTourismUseCase.run(UseCase.None())
 }
 
--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -1,14 +1,10 @@
 package com.dicoding.tourismapp.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
-import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.domain.GetAllTourismUseCase
-import com.dicoding.tourismapp.core.domain.Tourism
 import com.dicoding.tourismapp.core.domain.UseCase
 
 class HomeViewModel(private val getAllTourismUseCase: GetAllTourismUseCase) : ViewModel() {
-    fun getTourism(): LiveData<Resource<List<Tourism>>> = getAllTourismUseCase.run(UseCase.None())
+    fun getTourism() = getAllTourismUseCase.run(UseCase.None())
 }
 
