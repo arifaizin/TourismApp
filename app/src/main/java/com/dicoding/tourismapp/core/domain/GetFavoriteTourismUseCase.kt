691cf4d1c106d54ecce45a040733f312a2bf2bfa
Arif <faizin.arief@gmail.com>
2020-05-13 13:49:43 +0700

    Fix : clean up

--- app/src/main/java/com/dicoding/tourismapp/core/domain/GetFavoriteTourismUseCase.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/GetFavoriteTourismUseCase.kt
@@ -1,7 +1,6 @@
 package com.dicoding.tourismapp.core.domain
 
 import androidx.lifecycle.LiveData
-import com.dicoding.tourismapp.core.data.Resource
 
 class GetFavoriteTourismUseCase(private val tourismRepository: ITourismRepository) :
     UseCase<LiveData<List<Tourism>>, UseCase.None>() {
