d02434555ec37f1a82b20bef395be95f23edce63
Arif <faizin.arief@gmail.com>
2020-06-09 16:26:42 +0700

    Fix : remove unused code

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteFragment.kt
@@ -16,7 +16,6 @@ import com.dicoding.tourismapp.core.ui.TourismAdapter
 import com.dicoding.tourismapp.di.ViewModelFactory
 import com.dicoding.tourismapp.detail.DetailTourismActivity
 import kotlinx.android.synthetic.main.fragment_favorite.*
-import kotlinx.android.synthetic.main.fragment_home.rv_tourism
 import javax.inject.Inject
 
 class FavoriteFragment : Fragment() {
