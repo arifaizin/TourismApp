798198fd05c45bf329775643d4852caae838811b
Arif <faizin.arief@gmail.com>
2020-07-16 13:30:59 +0700

    Fix : inject itourismrepository

--- app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -7,13 +7,14 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.JsonHelper
 
 object Injection {
-    private fun provideRepository(context: Context): TourismRepository {
+    private fun provideRepository(context: Context): ITourismRepository {
         val database = TourismDatabase.getInstance(context)
 
         val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
