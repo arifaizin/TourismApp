aef8319040f30e0e52da254324a55fb8f398b321
Arif <faizin.arief@gmail.com>
2020-07-23 13:34:23 +0700

    Add: sqlcipher

--- core/src/main/java/com/dicoding/core/di/DatabaseModule.kt
+++ core/src/main/java/com/dicoding/core/di/DatabaseModule.kt
@@ -6,6 +6,8 @@ import com.dicoding.core.data.source.local.room.TourismDao
 import com.dicoding.core.data.source.local.room.TourismDatabase
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -13,11 +15,18 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(context: Context): TourismDatabase = Room.databaseBuilder(
-        context,
-        TourismDatabase::class.java, "Tourism.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(context: Context): TourismDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            TourismDatabase::class.java, "Tourism.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideTourismDao(database: TourismDatabase): TourismDao = database.tourismDao()
+
 }
