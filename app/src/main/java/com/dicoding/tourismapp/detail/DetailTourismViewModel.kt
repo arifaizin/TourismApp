32df061bd881f8e3afabe582b4a5c98684a76dd2
Arif <faizin.arief@gmail.com>
2020-08-04 15:56:11 +0700

    Update Icon

--- app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 
-class DetailTourismViewModel(val tourismRepository: TourismRepository) : ViewModel() {
+class DetailTourismViewModel(private val tourismRepository: TourismRepository) : ViewModel() {
     fun setFavoriteTourism(tourism: TourismEntity, newStatus:Boolean) = tourismRepository.setFavoriteTourism(tourism, newStatus)
 }
 
