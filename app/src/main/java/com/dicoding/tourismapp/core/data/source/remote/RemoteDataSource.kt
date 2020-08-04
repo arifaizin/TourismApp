550310e9cf47ae79ade9148e54388ec326f52d02
Arif <faizin.arief@gmail.com>
2020-08-04 16:46:12 +0700

    Add: retrofit2 direct

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -1,15 +1,12 @@
 package com.dicoding.tourismapp.core.data.source.remote
 
-import android.os.Handler
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
 import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class RemoteDataSource private constructor(private val apiService: ApiService) {
     companion object {
@@ -28,16 +25,16 @@ class RemoteDataSource private constructor(private val apiService: ApiService) {
         //get data from remote api
         val client = apiService.getList()
 
-        client.enqueue(object : Callback<ListTourismResponse> {
+        client.enqueue(object : retrofit2.Callback<ListTourismResponse> {
             override fun onResponse(
-                call: Call<ListTourismResponse>,
-                response: Response<ListTourismResponse>
+                call: retrofit2.Call<ListTourismResponse>,
+                response: retrofit2.Response<ListTourismResponse>
             ) {
                 val dataArray = response.body()?.places
                 resultData.value = if (dataArray != null) ApiResponse.Success(dataArray) else ApiResponse.Empty
             }
 
-            override fun onFailure(call: Call<ListTourismResponse>, t: Throwable) {
+            override fun onFailure(call: retrofit2.Call<ListTourismResponse>, t: Throwable) {
                 resultData.value = ApiResponse.Error(t.message.toString())
                 Log.e("RemoteDataSource", t.message.toString())
             }
