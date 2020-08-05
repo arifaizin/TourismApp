cb9423837eb50866138b5c04f5c36c73e6fbe935
Arif <faizin.arief@gmail.com>
2020-08-05 12:00:26 +0700

    Add annotation

--- app/src/main/java/com/dicoding/tourismapp/core/ui/ViewModelFactory.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/ui/ViewModelFactory.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import com.dicoding.tourismapp.detail.DetailTourismViewModel
+import com.dicoding.tourismapp.di.AppScope
 import com.dicoding.tourismapp.favorite.FavoriteViewModel
 import com.dicoding.tourismapp.home.HomeViewModel
 import javax.inject.Inject
 
+@AppScope
 class ViewModelFactory @Inject constructor(private val tourismUseCase: TourismUseCase) :
     ViewModelProvider.NewInstanceFactory() {
 
