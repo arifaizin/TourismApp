502aa97a6792422fe368f17dd1793e8adebf66c0
Arif <faizin.arief@gmail.com>
2024-08-28 13:49:08 +0700

    remove unused import

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/local/entity/TourismEntity.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/local/entity/TourismEntity.kt
@@ -1,10 +1,8 @@
 package com.dicoding.tourismapp.core.data.source.local.entity
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "tourism")
 data class TourismEntity(
--- app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismActivity.kt
+++ app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismActivity.kt
@@ -7,7 +7,6 @@ import androidx.core.content.IntentCompat.getParcelableExtra
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.dicoding.tourismapp.R
-import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.ui.ViewModelFactory
 import com.dicoding.tourismapp.databinding.ActivityDetailTourismBinding
