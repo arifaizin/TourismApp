f99ce8b548165cfb8ece3bddc3b0cf3b3e23cf4d
Arif <faizin.arief@gmail.com>
2024-09-13 13:55:24 +0700

    change to stateflow

--- app/src/main/java/com/dicoding/tourismapp/home/HomeFragment.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.tourismapp.R
 import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.ui.TourismAdapter
 import com.dicoding.tourismapp.databinding.FragmentHomeBinding
 import com.dicoding.tourismapp.detail.DetailTourismActivity
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
@@ -44,19 +48,21 @@ class HomeFragment : Fragment() {
                 startActivity(intent)
             }
 
-            homeViewModel.tourism.observe(viewLifecycleOwner) { tourism ->
-                if (tourism != null) {
-                    when (tourism) {
-                        is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
-                        is Resource.Success -> {
-                            binding.progressBar.visibility = View.GONE
-                            tourismAdapter.submitList(tourism.data)
-                        }
-                        is Resource.Error -> {
-                            binding.progressBar.visibility = View.GONE
-                            binding.viewError.root.visibility = View.VISIBLE
-                            binding.viewError.tvError.text =
-                                tourism.message ?: getString(R.string.something_wrong)
+            viewLifecycleOwner.lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    homeViewModel.tourism.collect { uiState ->
+                        when (uiState) {
+                            is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
+                            is Resource.Success -> {
+                                binding.progressBar.visibility = View.GONE
+                                tourismAdapter.submitList(uiState.data)
+                            }
+                            is Resource.Error -> {
+                                binding.progressBar.visibility = View.GONE
+                                binding.viewError.root.visibility = View.VISIBLE
+                                binding.viewError.tvError.text =
+                                    uiState.message ?: getString(R.string.something_wrong)
+                            }
                         }
                     }
                 }
--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -1,10 +1,19 @@
 package com.dicoding.tourismapp.home
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
-    val tourism = tourismUseCase.getAllTourism().asLiveData()
+    val tourism: StateFlow<Resource<List<Tourism>>> = tourismUseCase.getAllTourism().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = Resource.Loading()
+    )
 }
 
