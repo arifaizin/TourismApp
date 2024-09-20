d9cd7ab73b5885761b813594aac42045c9798042
Arif <faizin.arief@gmail.com>
2024-09-20 17:01:24 +0700

    revert obfuscation in module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -18,14 +18,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
